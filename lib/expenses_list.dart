8fc13f027d592ce5a83c053c1453a80fb177322b
Alessandra <tirnavanalessandra@gmail.com>
2023-08-16 18:11:17 +0200

    use ListView

---
+++ lib/expenses_list.dart
@@ -0,0 +1,21 @@
+import 'package:expenses_tracker/models/expense.dart';
+import 'package:flutter/material.dart';
+
+class ExpensesList extends StatelessWidget {
+  const ExpensesList({
+    required this.expenses,
+    super.key,
+  });
+
+  final List<Expense> expenses;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: expenses.length,
+      itemBuilder: (ctx, i) => Text(
+        expenses[i].title,
+      ),
+    );
+  }
+}
