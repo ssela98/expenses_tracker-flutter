52eaf971b3abe35330be165a6d4e31b53f561d6c
Alessandra <tirnavanalessandra@gmail.com>
2023-08-16 17:28:43 +0200

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ExpensesTracker());
+}
+
+class ExpensesTracker extends StatelessWidget {
+  const ExpensesTracker({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Expenses Tracker',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Scaffold(
+        body: Text('Hola!'),
+      ),
+    );
+  }
+}
