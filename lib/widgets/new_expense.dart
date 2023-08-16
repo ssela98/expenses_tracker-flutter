c6d55eae5408bcde356481b52ff54038b37556ed
Alessandra <tirnavanalessandra@gmail.com>
2023-08-16 20:57:59 +0200

    store user input in local variable

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -10,17 +10,32 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  String _enteredTitle = '';
+
+  void _saveTitleInput(String inputValue) {
+    _enteredTitle = inputValue;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(16),
+    return Padding(
+      padding: const EdgeInsets.all(16),
       child: Column(
         children: [
           TextField(
+            onChanged: _saveTitleInput,
             maxLength: 50,
-            decoration: InputDecoration(label: Text('Title')),
+            decoration: const InputDecoration(label: Text('Title')),
             autofocus: true,
-          )
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Submit'),
+              ),
+            ],
+          ),
         ],
       ),
     );
