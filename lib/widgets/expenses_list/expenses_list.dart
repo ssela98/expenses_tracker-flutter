fc8e9c8e0c6d3594df98e3591f30a0ab404a3438
Alessandra <tirnavanalessandra@gmail.com>
2023-08-18 09:40:23 +0200

    add red background to swipeables

--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -1,3 +1,4 @@
+import 'package:expenses_tracker/main.dart';
 import 'package:expenses_tracker/models/expense.dart';
 import 'package:expenses_tracker/widgets/expenses_list/expense_item.dart';
 import 'package:flutter/material.dart';
@@ -18,6 +19,10 @@ class ExpensesList extends StatelessWidget {
       itemCount: expenses.length,
       itemBuilder: (ctx, i) => Dismissible(
         key: ValueKey(expenses[i]),
+        background: Container(
+          color: kColorScheme.error,
+          margin: Theme.of(context).cardTheme.margin,
+        ),
         onDismissed: (direction) {
           removeExpense(expenses[i]);
         },
